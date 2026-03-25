731644235f40e10835485292e330db56e4bca7de
DaviMendonca <davimelo2111@gmail.com>
2026-03-25 12:47:44 -0300

    correção do formato data no df saldo_mensal

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ st.text("Este é um exemplo de controle financeiro usando Pandas e Streamlit." \
 " processados e exibidos em um formato amigável.")
 
 col_esquerda, col_central_direita = st.columns([1, 2])
-
+'testegi'
 with col_central_direita:
     st.text(f"Saldo Atual: {saldo_total}")
     df_exibicao = df.fillna('')
